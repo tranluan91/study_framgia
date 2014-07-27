05252d8e7011376a53dae265500c36d81ae85d97
Tran Van Luan <tran.van.luan@framgia.com>
2014-07-27 10:01:24 +0700

    migration database

---
+++ migrations/1.0.0/products.php
@@ -0,0 +1,58 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class ProductsMigration_100 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'products',
+            array(
+            'columns' => array(
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'name',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 32,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'price',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'name'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id'))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '1',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_unicode_ci'
+            )
+        )
+        );
+    }
+}
